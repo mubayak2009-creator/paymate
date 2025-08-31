de34e30711e7fb0556715cfafbbac8193b223f4f
mubayak2009-creator <mubayak2009@gmail.com>
2025-08-31 10:39:56 +0000

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,24 +1,21 @@
-// firebase.js
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Import Firebase SDK
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
-
-// Your Firebase config (replace with your own from Firebase Console)
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCM7__CBDBWAOH8c3TKShNEOyG-Pq5NDMs",
+  authDomain: "paymate-e4949.firebaseapp.com",
+  projectId: "paymate-e4949",
+  storageBucket: "paymate-e4949.firebasestorage.app",
+  messagingSenderId: "930809633379",
+  appId: "1:930809633379:web:d10f7f62f1243dfdc5f656",
+  measurementId: "G-EVVG2TCZEC"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Export auth + db
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-
+const analytics = getAnalytics(app);
